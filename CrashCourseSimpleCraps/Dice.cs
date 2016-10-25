b8bc011f78ef45c4272b78e4f7e36ade96912175
Greg Doud <gpdoud@gmail.com>
2016-10-25 15:33:18 -0400

    Inital commit

---
+++ CrashCourseSimpleCraps/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourseSimpleCraps {
+    public class Dice {
+        public int Dice1 { get; private set; } = 0;
+        public int Dice2 { get; private set; } = 0;
+        public int Total { get; private set; } = 0;
+
+        public static Dice Roll() {
+            Console.WriteLine("Press any key to roll the dice...");
+            Console.ReadKey();
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            Dice dice = new Dice();
+            dice.Dice1 = rnd.Next(1, 6);
+            dice.Dice2 = rnd.Next(1, 6);
+            dice.Total = dice.Dice1 + dice.Dice2;
+            Console.WriteLine($"Roll is {dice.Total}");
+            return dice;
+       }
+    }
+}
---
+++ CrashCourseSimpleCraps/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrashCourseSimpleCraps {
+    class Program {
+        static void Main(string[] args) {
+            Program pgm = new Program();
+            pgm.Run();
+        }
+
+        private void Run() {
+            Console.WriteLine("Simple Craps - A Crash Course from The Iron Yard");
+            bool playAgain = true;
+            while(playAgain) {
+
+                PlaySimpleCraps();
+
+                playAgain = AskPlayAgain();
+            }
+        }
+
+        private void PlaySimpleCraps() {
+            var thePoint = 0;
+            Dice dice = Dice.Roll();
+            while(!IsWinnerOrLoser(dice, thePoint)) {
+                if(thePoint == 0) {
+                    thePoint = dice.Total;
+                    Console.WriteLine($"The point is {dice.Total}");
+                }
+                dice = Dice.Roll();
+            }
+            if(IsWinner(dice, thePoint)) {
+                Console.WriteLine("You Win!!!");
+            } if(IsLoser(dice, thePoint)) {
+                Console.WriteLine("You Lose ..."); 
+            }
+        }
+
+        private bool IsWinnerOrLoser(Dice dice, int thePoint) {
+            return IsWinner(dice, thePoint) || IsLoser(dice, thePoint);
+        }
+
+        private bool IsLoser(Dice dice, int thePoint) {
+            return (thePoint == 0) ? dice.Total == 2 || dice.Total == 3 || dice.Total == 12 : dice.Total == 7;
+        }
+
+        private bool IsWinner(Dice dice, int thePoint) {
+            return (thePoint == 0) ? dice.Total == 7 : dice.Total == thePoint;
+        }
+
+        private bool AskPlayAgain() {
+            Console.WriteLine("Do you want to play again? (N to quit) ");
+            var answer = Console.ReadKey(true);
+            return answer.KeyChar.ToString().ToUpper() != "N";
+        }
+    }
+}
